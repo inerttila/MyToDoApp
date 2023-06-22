7905ca3a9225fe10cdf8e04bd4ff99986afd481e
SKAITECH <skaitech@Arlas-MacBook-Pro.local>
2023-06-22 16:08:01 +0100

    Initial commit

---
+++ todo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+
+admin.site.register(Todo, TodoAdmin)
